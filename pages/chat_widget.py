8e649e42cf2b7121a0e8ba310e25fb993c6f8019
Alena Gushcha <g.aliena@mail.ru>
2026-04-06 13:08:36 +0300

    added test_capabilities_texts.py

--- pages/chat_widget.py
+++ pages/chat_widget.py
@@ -4,6 +4,7 @@ from selene import browser, be, have
 class ChatWidget:
     IFRAME = 'iframe.tutu-chat-widget-iframe'
     WELCOME_TITLES = 'h2[data-ti="heading-2"]'
+    CAPABILITY_LABELS = 'span[data-ti="label-value-label"]'
 
     def should_have_welcome_message(self):
         browser.element(self.IFRAME).should(be.visible)
@@ -14,6 +15,25 @@ class ChatWidget:
         ).should(be.visible).should(
             have.text('Знаю всё про путешествия')
         )
+
+        browser.driver.switch_to.default_content()
+        return self
+
+    def should_have_capabilities_texts(self):
+        browser.element(self.IFRAME).should(be.visible)
+        browser.driver.switch_to.frame(browser.element(self.IFRAME).locate())
+
+        browser.all(self.CAPABILITY_LABELS).element_by(
+            have.text('Предложу идеи для отпуска: куда съездить, что посмотреть')
+        ).should(be.visible)
+
+        browser.all(self.CAPABILITY_LABELS).element_by(
+            have.text('Спланирую ваше путешествие: подскажу маршрут, жильё, билеты')
+        ).should(be.visible)
+
+        browser.all(self.CAPABILITY_LABELS).element_by(
+            have.text('Помогу с обменом и возвратом заказа. Если надо, позову оператора')
+        ).should(be.visible)
 
         browser.driver.switch_to.default_content()
         return self
---
+++ tests/test_capabilities_texts.py
@@ -0,0 +1,11 @@
+from pages.main_page import MainPage
+from pages.chat_widget import ChatWidget
+
+
+def test_user_can_see_jarvel_capabilities_texts():
+    main_page = MainPage()
+    chat_widget = ChatWidget()
+
+    main_page.open()
+    main_page.open_jarvel_chat()
+    chat_widget.should_have_capabilities_texts()
